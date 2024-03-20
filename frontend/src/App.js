5d38424f04ea23fef342ffe9bac4fd6daeef8fcb
jduda27 <joshua.duda@gmail.com>
2024-03-20 14:56:13 -0500

    Fixed Docker Compose for all machines, added Styling and basic homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,12 @@ function App() {
     <div className="App">
       <div className="bg-hero bg-cover bg-center pb-80">
         <Header />
-        <h1 className="mt-20 ml-20 text-8xl font-bold text-left">THE NEXT AGE OF SPACE SIGHTSEEING</h1>
+        <h1 className="mt-20 ml-10 mr-10 text-6xl font-bold text-left line-clamp-3">THE NEW AGE OF SPACE EXPLORATION</h1>
         <p className="ml-20 mt-8 text-2xl text-left">Look up and look upon the stars.</p>
         <button className=" flex ml-20 mt-8 bg-orange-600 text-black hover:bg-orange-300 text-xl font-bold p-4 rounded-lg ">View the Calendar</button>
       </div>
       <div className="mt-20 bg-black pb-80">
-        <h1 className="ml-20 text-8xl font-bold text-center">THE NEXT AGE OF SPACE SIGHTSEEING</h1>
+        <h1 className="ml-20 text-8xl font-bold text-center">THE NEW AGE OF SPACE SIGHTSEEING</h1>
         <p className="ml-20 mt-8 text-2xl text-center underline">Look up and look upon the stars.</p>
         <button className=" ml-20 mt-8 bg-orange-600 text-black hover:bg-orange-300 text-xl font-bold p-4 rounded-lg ">Signup Now</button>
       </div>
--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,23 +1,65 @@
 import React from 'react';
+import Hamburger from './Hamburger';
 
 function Header() {
-return (
-    <div className="Header-nav">
-    <div className="flex flex-row p-5">
-        <div className="basis-1/4">
-            <a className="text-6xl font-bold" href="/">Kepler .</a>
+
+    const [hamburgerOpen, setHamburgerOpen] = React.useState(false);
+
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen);
+    }
+
+    return (
+        <div className="Header-nav">
+            <div className="flex flex-row p-5">
+                <div className="basis-1/4">
+                    <a className="text-6xl font-bold" href="/">Kepler.</a>
+                </div>
+                <div className="navigation">
+                    <ul>
+                        <li>Home</li>
+                        <li>Calendar</li>
+                        <li>My Account</li>
+                    </ul>
+                </div>
+                <div className="hamburger" onClick={toggleHamburger}>
+                    <Hamburger />
+                </div>
+            </div>
+
+            <style jsx>{`
+        .navigation{
+            width: 100%;
+            height:50px;
+        }
+        .navigation ul{
+            display: flex;
+            flex-wrap: wrap;
+            float: right;
+            margin: 20 0px;
+            padding: 0 25px;
+        }
+
+        .navigation ul {
+            display: ${hamburgerOpen ? 'inline' : 'none'};
+            background-color: black;
+            height: 100vh;
+            width: 50vw;
+            margin-top: 50px;
+            position: absolute;
+        }
+
+        .navigation ul li {
+            list-style-type: none;
+            padding-right: 10px;
+        }
+        .hamburger{
+            display: flex;
+            z-index: 10;
+        }
+    `}</style>
         </div>
-        <div className="basis-3/4"></div>
-        <div className="p-2 ml-6 basis-1/4">
-            <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 18L20 18" stroke="#FFFF" stroke-width="2" stroke-linecap="round"/>
-                <path d="M4 12L20 12" stroke="#FFFF" stroke-width="2" stroke-linecap="round"/>
-                <path d="M4 6L20 6" stroke="#FFFF" stroke-width="2" stroke-linecap="round"/>
-            </svg>
-        </div>
-    </div>
-    </div>
-)
+    )
 }
 
 export default Header;
