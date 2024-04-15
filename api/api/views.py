4bd3b30d374c22f50ec329fb82baf456e3998226
Alex Pizarro <87215633+RazzMatazz5874@users.noreply.github.com>
2024-04-15 17:18:56 -0500

    Update views.py
    updated api

--- api/api/views.py
+++ api/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from astronomy.views import *
 
-# Create your views here.
+
+def astronomy_data(request):
+    year, month, day = 2024, 4, 10  # example date
+    # example coordinates for New York City
+    latitude, longitude = 40.7128, -74.0060
+
+    sunrise_utc = calculate_celestial_body_rise_utc(
+        "Sun", year, month, day, latitude, longitude)[0]
+    sunset_utc = calculate_celestial_body_set_utc(
+        "Sun", year, month, day, latitude, longitude)[0]
+    culmination_utc = calculate_celestial_body_culmination_utc(
+        "Sun", latitude, longitude, year, month, day)
+    twilight_end_utc = calculate_astronomical_twilight_end_utc(
+        year, month, day, latitude, longitude)
+
+    data = {
+        "sunrise": sunrise_utc,
+        "sunset": sunset_utc,
+        "culmination": culmination_utc,
+        "twilight_end": twilight_end_utc
+    }
+
+    return JsonResponse(data)
