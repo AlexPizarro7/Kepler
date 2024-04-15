b799d7e9aca4387ba9da4f2e90a00bd3907b2660
Alex Pizarro <87215633+RazzMatazz5874@users.noreply.github.com>
2024-04-15 17:23:25 -0500

    Update urls.py
    update api

--- api/backend/urls.py
+++ api/backend/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, astronomy_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('astronomy-data/', astronomy_data, name='astronomy-data'),
 ]
