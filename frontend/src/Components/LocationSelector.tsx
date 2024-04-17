dd91661d536a00ed72d6f389c3c4cc1aadbc583a
jduda27 <joshua.duda@gmail.com>
2024-04-16 20:04:16 -0500

    Updated Calendar and calendar selector

---
+++ frontend/src/Components/LocationSelector.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import {
+    CitySelect,
+    CountrySelect,
+    StateSelect,
+    LanguageSelect,
+} from "react-country-state-city";
+import "react-country-state-city/dist/react-country-state-city.css";
+
+
+const LocationSelector = () => {
+    const [countryid, setCountryid] = useState(0);
+    const [stateid, setstateid] = useState(0);
+
+    return (
+        <form className="ml-20 mr-20 bg-gradient-to-r from-slate-900 to-slate-700 p-[20px] flex flex-col justify-center rounded">
+            <label>Country</label>
+            <div className="text-black bg-gradient-to-r from-slate-500 to-slate-800">
+                <CountrySelect
+                    onChange={(e) => {
+                        setCountryid(e.id);
+                    }}
+                    placeHolder="Select Country"
+                />
+            </div>
+            <div className="mt-10 flex w-[80%]">
+                <label className="mr-10">State</label>
+                <div className="text-black bg-gradient-to-r from-slate-500 to-slate-800">
+                    <StateSelect
+                        countryid={countryid}
+                        onChange={(e) => {
+                            setstateid(e.id);
+                        }}
+                        placeHolder="Select State"
+                    />
+                </div>
+                <label className="ml-10 mr-10">City</label>
+                <input className="text-black rounded p-[5px] w-[200px]" type="text" placeholder="City" />
+            </div>
+
+            <button className="bg-gray-900 text-white hover:bg-blue-900 text-xl font-bold p-4 rounded-lg mt-4">Get Celestial Events</button>
+            <style>
+                {`
+                    .stdropdown-menu:empty {
+                        display: none;
+                    }
+                `}
+            </style>
+        </form>
+    );
+
+}
+
+export default LocationSelector;
--- frontend/src/Pages/Calendar.tsx
+++ frontend/src/Pages/Calendar.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import CalendarInformation from '../Components/CalendarInformation';
 import CalendarSelector from '../Components/CalendarSelector';
 import RequestAstronomy from '../AstronomyAPI';
+import LocationSelector from '../Components/LocationSelector';
 
 
 
@@ -19,11 +20,27 @@ const Calendar = () =>  {
         <div className="content-wrapper">
             <div className="">
                 <h1>Calendar</h1>
+                <div className="mb-10">  
+                    <LocationSelector />
+                </div>
                 <CalendarInformation data={data}/>
-                <div className="h-[95vh] m-10 bg-white text-black">
-                <CalendarSelector />
+                <div className="h-[95vh] m-10 bg-gradient-to-r from-slate-300 to-slate-500 text-black">
+                    <CalendarSelector />
                 </div>
             </div>
+            <style>
+                {`
+                    .rbc-calendar {
+                        border: 1px solid #fff;
+                    }
+                    .rbc-today {
+                        background-color:lightblue;
+                    }
+                    .rbc-off-range-bg {
+                        background-color: #616283;
+                    }
+                `}
+            </style>
         </div>
     )
 }
